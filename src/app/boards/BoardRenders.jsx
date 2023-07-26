9f4bd8a1d169d809da0f190480b293db6837f80d
KeinoC <keino.chichester@gmail.com>
2023-07-25 22:33:31 -0400

    job board route setup

---
+++ src/app/boards/BoardRenders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+export function RenderBoardCards() {
+    return (
+        <div>
+            <h1>Job Cards</h1>
+        </div>
+    );
+}
---
+++ src/app/boards/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { RenderBoardCards} from "./BoardRenders"
+
+export const BoardPage = () => {
+    return (
+        <div>
+            <RenderBoardCards />
+        </div>
+    )
+}
