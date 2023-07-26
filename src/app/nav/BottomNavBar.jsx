543ea0c72641fccd95d7878167448a0575d8433a
KeinoC <keino.chichester@gmail.com>
2023-07-25 23:20:14 -0400

    job board route setup

--- src/app/nav/BottomNavBar.jsx
+++ src/app/nav/BottomNavBar.jsx
@@ -4,8 +4,8 @@ export default function BottomNavBar() {
     return (
         <div className="overflow-x-hidden w-screen left-0 absolute bottom-20 justify-center item-center p-0 m-0 ">
             <div className="item-center flex flex-row max-w-screen justify-around">
-                <button class="w-1/3 bg-slate-100 hover:bg-yellow-700 text-xs text-slate-900 font-bold py-2 px-4 mx-1 rounded-full">Job Board</button>
-                <button class="w-1/3 bg-slate-100 hover:bg-yellow-700 text-xs text-slate-900 font-bold py-2 px-4 mx-1 rounded-full">Dev Directory</button>
+                <button className="w-1/3 bg-slate-100 hover:bg-yellow-700 text-xs text-slate-900 font-bold py-2 px-4 mx-1 rounded-full">Job Board</button>
+                <button className="w-1/3 bg-slate-100 hover:bg-yellow-700 text-xs text-slate-900 font-bold py-2 px-4 mx-1 rounded-full">Dev Directory</button>
             </div>
         </div>
     );
