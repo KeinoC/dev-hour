00d566c29990ff8f889258a59f0592a5bee8c83a
KeinoC <keino.chichester@gmail.com>
2023-07-26 21:51:09 -0400

    spent way too much time on shjob board cards but looks okay and links

--- src/app/boards/BoardRenders.jsx
+++ src/app/boards/BoardRenders.jsx
@@ -1,12 +1,53 @@
+"user client"
 import React from "react";
+import { BsLinkedin, BsBook } from "react-icons/bs";
+import "./board.css"
+
+const jobBoardsArray = [
+    {
+        id: 1,
+        name: "LinkedIn",
+        url: "https://www.linkedin.com/jobs/search/?f_LF=f_AL&geoId=103644278&keywords=software%20engineer&location=United%20States",
+        rating: 0,
+        icon: "IN",
+        description: "testing linkedin description"
+    },
+    {
+        id: 2,
+        name: "Career Builder",
+        url: "https://www.careerbuilder.com/jobs-software-engineer",
+        rating: 0,
+        icon: "CB",
+        description: "testing cb description"
+    }
+];
 
 
+
+const renderJobBoards = jobBoardsArray.map((jobBoard) => {
+    return (
+        <div className="board-card">
+            <h1>{jobBoard.name}</h1>
+            <a href={jobBoard.url}>
+                <div className="icon-wrapper">
+                    <div className="icon">
+
+                    {jobBoard.icon}
+                    </div>
+                    <div className="description">
+
+                    {jobBoard.description}
+                    </div>
+                </div>
+            </a>
+        </div>
+    );
+})
 
 
 export function RenderBoardCards() {
-    return (
-        <div>
-            <h1>Job Cards</h1>
-        </div>
+    return (<>
+{renderJobBoards}
+    </>
     );
 }
